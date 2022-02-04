1c001f74d97e9308341df4a4fe8348a1afbdc632
DeLuxe1337001 <rragtagbates@gmail.com>
2022-02-04 12:40:10 -0500

    Push

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+mod vm;
+
+use vm::VM;
+use std::io::Read;
+
+fn main() {
+    let mut c = VM::new();
+
+    let mut file = std::fs::File::open("program.bin").expect("Expected file");
+
+    let mut program = [0; 4096];
+
+    file.read(&mut program).expect("Expected read");
+
+    let converted = program.iter().map(|&x| x as i32).collect::<Vec<i32>>();
+
+    c.load(converted.as_slice());
+
+    /*c.load(&[
+        0x2, 100, 0,
+        0x2, 200, 1,
+        0x5, 0, 1,
+        0x10, 0x0, 0
+    ]);*/
+
+    c.run();
+    c.dump(25, 5);
+}
+
+/*
+
+!--Registers--!
+
+? You can use up to 1000 registers.
+
+!-------------!
+
+* 0x1 = nop
+* 0x2 = push num, register
+* 0x3 = jmp num
+* 0x4 = jmp if reg = num (jmp to, reg, num)
+
+* 0x5 = add reg,reg
+* 0x6 = sub
+* 0x7 = mul
+* 0x8 = div
+
+* 0x10 = print mode, reg
+
+? Print_Modes 
+    0x0 = print reg
+    0x1 = print constant
+
+*/
---
+++ src/vm.rs
@@ -0,0 +1,165 @@
+pub struct VM {
+    pub memory: [i32; 4096],
+    pub registers: [i32; 1000],
+    pub constants: Vec<String>,
+    pub pc: usize
+}
+
+impl VM { 
+    pub fn new() -> VM {
+        return VM {
+            memory: [0; 4096],
+            registers: [0; 1000],
+            constants: Vec::new(),
+            pc: 0,
+        };
+    }
+    pub fn load(&mut self, program: &[i32]) {
+        for (i, &byte) in program.iter().enumerate() {
+            self.memory[i] = byte;
+        }
+    }
+    pub fn new_constant(&mut self, name: &str) {
+        self.constants.push(name.to_string());
+    }
+    pub fn dump(&self, to: usize, reg: usize) {
+        println!("--Debug----------------------------------------------------");
+        println!("--Registers--");
+
+        for (i, b) in self.registers.iter().enumerate() {
+            if i == reg {
+                println!(";");
+                break;
+            }
+
+            print!("Register {0} = {1} ", i, b);
+        }
+
+        println!("--Memory--");
+
+        for (i, b) in self.memory.iter().enumerate() {
+            if i == to {
+                println!(";");
+                break;
+            }
+
+            print!("{} ", b);
+        }
+
+        println!("-----------------------------------------------------------");
+    }
+    fn step(&mut self) {
+        self.pc += 1;
+    }
+    fn next(&mut self) -> i32 {
+        self.step();
+        return self.memory[self.pc];
+    }
+    fn setr(&mut self, register: usize, value: i32) {
+        self.registers[register] = value;
+    }
+    pub fn run(&mut self) {
+        while self.pc < self.memory.len() {
+            let op = self.memory[self.pc];
+
+            //println!("PC: 0x{:x}", self.pc);
+
+            match op {
+                0x1 | 0x0 => {
+                    self.step();
+                },
+                0x2 => {
+                    let value = self.next();
+                    let register = self.next() as usize;
+                    
+                    self.setr(register, value);
+
+                    self.step();
+
+                    println!("push {1}, reg({0})", register, value);
+                },
+                0x3 => {
+                    let value = self.next();
+                    self.pc = value as usize;
+
+                    println!("jmp 0x{:x}", value);
+                },
+                0x4 => {
+                    let jmp = self.next() as usize;
+                    let reg = self.next() as usize;
+                    let value = self.next();
+
+                    if self.registers[reg] == value {
+                        self.pc = jmp;
+                    }
+
+                    println!("jmpeq 0x{1:x} if reg({0}) == {2}", reg, jmp, value);
+                },
+                0x5 => {
+                    let register_a = self.next() as usize;
+                    let register_b = self.next() as usize;
+
+                    self.setr(register_a, self.registers[register_a] + self.registers[register_b]);
+                    self.setr(register_b, 0x0);
+
+                    self.step();
+
+                    println!("add reg({0}), reg({1})", register_a, register_b);
+                },
+                0x6 => {
+                    let register_a = self.next() as usize;
+                    let register_b = self.next() as usize;
+
+                    self.setr(register_a, self.registers[register_a] - self.registers[register_b]);
+                    self.setr(register_b, 0x0);
+
+                    self.step();
+
+                    println!("sub reg({0}), reg({1})", register_a, register_b);
+                },
+                0x7 => {
+                    let register_a = self.next() as usize;
+                    let register_b = self.next() as usize;
+
+                    self.setr(register_a, self.registers[register_a] * self.registers[register_b]);
+                    self.setr(register_b, 0x0);
+
+                    self.step();
+
+                    println!("mul reg({0}), reg({1})", register_a, register_b);
+                },
+                0x8 => {
+                    let register_a = self.next() as usize;
+                    let register_b = self.next() as usize;
+
+                    self.setr(register_a, self.registers[register_a] / self.registers[register_b]);
+                    self.setr(register_b, 0x0);
+
+                    self.step();
+
+                    println!("div reg({0}), reg({1})", register_a, register_b);
+                },
+                0x10 => {
+                    let mode = self.next();
+                    let register = self.next() as usize;
+
+                    match mode {
+                        0x0 => {
+                            println!("{}", self.registers[register]);
+                        }
+                        0x1 => {
+                            println!("{}", self.constants[self.registers[register] as usize]);
+                        }
+                        _ => {}
+                    }
+
+                    println!("print mode({0}) reg({1})", mode, register);
+                },
+                _ => {
+                    println!("Unknown opcode: 0x{:x}", op);
+                    self.step();
+                }
+            }
+        }
+    }
+}
